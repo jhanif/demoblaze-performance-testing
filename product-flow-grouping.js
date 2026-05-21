8d521703ba3e418cabde9ff4425b8ad5b5b444e1
jhanif <jalaluddinhanif28@gmail.com>
2026-05-21 22:45:14 +0700

    tags and grouping homepage and productpage

---
+++ product-flow-grouping.js
@@ -0,0 +1,37 @@
+import { group, check, sleep } from "k6";
+import http from 'k6/http';
+
+export const options = {
+    stages: [
+        { duration: '30s', target: 10 },
+        { duration: '1m', target: 20 },
+        { duration: '20s', target: 0 },
+    ],
+        thresholds: {
+        'http_req_duration{name:Homepage}': ['p(95)<500'],
+        'http_req_duration{name:ProductPage}': ['p(95)<800'],
+    }
+};
+
+export default function () {
+    group('Homepage Flow', function (){
+        let homeRes = http.get('https://demoblaze.com', {
+            tags: { name: 'Homepage' },
+        });
+
+        check(homeRes, {
+            'homepage loaded': (r) => r.status === 200,
+        });
+        sleep(2);
+    });
+    group('Productpage Flow',function(){
+        let productRes = http.get('https://www.demoblaze.com/prod.html?idp_=1', {
+            tags: { name: 'ProductPage' },
+        });
+
+        check(productRes, {
+            'productpage loaded': (r) => r.status === 200,
+        });
+        sleep(2);
+    });
+}
